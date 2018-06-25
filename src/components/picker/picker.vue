e47956c76d4f43e1ca788dbbf14f64bad7e0c102
yuangong <yuangong@mac-mini-3.local>
2018-06-25 15:07:46 +0800

    picker组件基本功能实现

--- src/components/picker/picker.vue
+++ src/components/picker/picker.vue
@@ -1,16 +1,226 @@
 <template>
-<div>
-    Welcome to picker
-</div>
+    <transition name='picker-fade'>
+        <div class='picker' v-show='state === 1'>
+            <transition name='picker-move'>
+                <div class='picker-panel' v-show='state === 1'>
+                    <div class='picker-choose border-bottom-1px'>
+                        <div class='cancelBtn' v-text='cancelTxt'></div>
+                        <div class='title' v-text='title'></div>
+                        <div class='confirmBtn' v-text='confirmTxt'></div>
+                    </div>
+                    <div class='picker-content'>
+                        <div class='mask-top border-bottom-1px'></div>
+                        <div class='mask-bottom border-top-1px'></div>
+                        <div class='wheel-wrapper' ref='wheelWrapper'>
+                            <div class='wheel' v-for='(wheel, index) in pickerData' :key='index'>
+                                <ul class='wheel-scroll'>
+                                    <li class='wheel-item' v-for='(item, index) in wheel' :key='index' v-text='item.text'></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class='picker-footer'></div>
+                </div>
+            </transition>
+        </div>
+    </transition>
 </template>
 
 <script>
+import BetterScroll from 'better-scroll'
+
 const COMPONENT_NAME = 'picker'
 
+const STATE_SHOW = 1
+const STATE_HIDDEN = 0
+const EVENT_CHANGE = 'change'
+
 export default {
-    name: COMPONENT_NAME
+    name: COMPONENT_NAME,
+    props: {
+        data: {
+            type: Array,
+            default: function () {
+                return []
+            }
+        },
+        selectedIndex: {
+            type: Array,
+            default: function () {
+                return []
+            }
+        },
+        cancelTxt: {
+            type: String,
+            default: 'cancel'
+        },
+        confirmTxt: {
+            type: String,
+            default: 'confirm'
+        },
+        title: {
+            type: String,
+            default: ''
+        }
+    },
+    data () {
+        return {
+            state: STATE_HIDDEN,
+            pickerData: this.data.slice(),
+            pickerSelectedIndex: this.selectedIndex
+        }
+    },
+    methods: {
+        show () {
+            if (this.state) {
+                return
+            }
+            this.state = STATE_SHOW
+            if (!this.wheels || this.dirty) {
+                this.$nextTick(() => {
+                    const wheelWrapper = this.$refs.wheelWrapper
+                    for (let i = 0; i < this.pickerData.length; i++) {
+                        this._createWheels(wheelWrapper, i)
+                    }
+                    this.dirty = false
+                })
+            } else {
+                for (let i = 0; i < this.pickerData.length; i++) {
+                    this.wheels[i].enable()
+                    this.wheels[i].wheelTo(this.pickerSelectedIndex[i])
+                }
+            }
+        },
+        _createWheels (wheelWrapper, i) {
+            if (!this.wheels) {
+                this.wheels = []
+            }
+            if (!this.wheels[i]) {
+                this.wheels[i] = new BetterScroll(wheelWrapper.children[i], {
+                    wheel: {
+                        selectedIndex: this.pickerSelectedIndex[i],
+                        wheelWrapperClass: 'wheel-scroll',
+                        wheelItemClass: 'wheel-item'
+                    },
+                    probeType: 3
+                })
+                this.wheels[i].on('scrollEnd', () => {
+                    this.$emit(EVENT_CHANGE, i, this.wheels[i].getSelectedIndex())
+                })
+            } else {
+                this.wheels[i].refresh()
+            }
+            return this.wheels[i]
+        }
+    },
+    created () {
+        if (this.pickerSelectedIndex || this.pickerSelectedIndex.length <= 0) {
+            this.pickerSelectedIndex = []
+            for (let i = 0; i < this.pickerData.length; i++) {
+                this.pickerSelectedIndex[i] = 0
+            }
+        }
+        this.wheels = null // 每一个齿轮
+        this.dirty = true // 是否有数据改动，如果数据改动了，就设置为true，否则设置为false
+    }
 }
 </script>
 
 <style lang='less' scoped>
+@import '../../commons/less/variable.less';
+@import '../../commons/less/base.less';
+@import '../../commons/less/mixin.less';
+
+.picker {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 100;
+    overflow: hidden;
+    text-align: center;
+    font-size: @fontsize-medium;
+    background: @color-mask-bgc;
+    .picker-panel {
+        position: absolute;
+        bottom: 0;
+        z-index: 600;
+        width: 100%;
+        height: 273px;
+        background-color: @color-white;
+        .picker-choose {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            height: 60px;
+            color: @color-light-grey;
+            padding: 0 16px;
+            .title {
+                color: @color-dark-grey;
+                font-size: @fontsize-large-x;
+            }
+            .confirmBtn {
+                color: @color-main;
+                font-size: @fontsize-medium;
+                &.active {
+                    color: @color-main-ll;
+                }
+            }
+            .cancel {
+                font-size: @fontsize-medium;
+                &.active {
+                    color: @color-active-light-gray-fe;
+                }
+            }
+        }
+        .picker-content {
+            position: relative;
+            top: 20px;
+            .mask-top, .mask-bottom {
+                z-index: 10;
+                width: 100%;
+                height: 68px;
+                pointer-events: none;
+                transform: translateZ(0)
+            }
+            .mask-top {
+                position: absolute;
+                top: 0;
+                background: linear-gradient(to top, rgba(255, 255, 255, 0.4), rgba(255, 255, 255, 0.8));
+            }
+            .mask-bottom {
+                position: absolute;
+                bottom: 0;
+                background: linear-gradient(to bottom, rgba(255, 255, 255, 0.4), rgba(255, 255, 255, 0.8));
+            }
+            .wheel-wrapper {
+                display: flex;
+                padding: 0 16px;
+                .wheel {
+                    .flex-fix;
+                    height: 173px;
+                    overflow: hidden;
+                    font-size: @fontsize-large-xx;
+                    .wheel-scroll {
+                        padding: 0;
+                        margin-top: 68px;
+                        line-height: 36px;
+                        list-style: none;
+                        .wheel-item {
+                            list-style: none;
+                            height: 36px;
+                            overflow: hidden;
+                            white-space: nowrap;
+                            color: @color-dark-grey;
+                        }
+                    }
+                }
+            }
+        }
+        .picker-footer {
+            height: 20px;
+        }
+    }
+}
 </style>
--- src/pages/picker/index.vue
+++ src/pages/picker/index.vue
@@ -1,9 +1,9 @@
 <template>
-    <page :title="$t('examples.picker')"
+    <page :title="$t('examples.picker')"  class='picker-view'
         :desc="$t('pickerPage.desc')">
         <div slot='content'>
             <div class='select' @click='showPicker(0)' ref='select0' v-text='selectedText[0]'></div>
-            <picker :data='[data[0]]'></picker>
+            <picker :data='data[0]' :title='title[0]' ref='picker0'></picker>
         </div>
     </page>
 </template>
@@ -18,14 +18,25 @@ export default {
     data () {
         return {
             selectedText: [
-                this.$i18n.t('pickerPage.oneColumn') + this.$i18n.t('pickerPage.pickDemo'),
-                this.$i18n.t('pickerPage.twoColumn') + this.$i18n.t('pickerPage.pickDemo'),
-                this.$i18n.t('pickerPage.threeColumn') + this.$i18n.t('pickerPage.pickDemo'),
-                this.$i18n.t('pickerPage.linkColumn') + this.$i18n.t('pickerPage.pickDemo')
+                this.$i18n.t('pickerPage.oneColumn') + this.$i18n.t('pickerPage.pickerDemo'),
+                this.$i18n.t('pickerPage.twoColumn') + this.$i18n.t('pickerPage.pickerDemo'),
+                this.$i18n.t('pickerPage.threeColumn') + this.$i18n.t('pickerPage.pickerDemo'),
+                this.$i18n.t('pickerPage.linkColumn') + this.$i18n.t('pickerPage.pickerDemo')
             ], 
+            title: [
+                this.$i18n.t('pickerPage.oneColumn') + this.$i18n.t('pickerPage.picker'),
+                this.$i18n.t('pickerPage.twoColumn') + this.$i18n.t('pickerPage.picker'),
+                this.$i18n.t('pickerPage.threeColumn') + this.$i18n.t('pickerPage.picker'),
+                this.$i18n.t('pickerPage.linkage') + this.$i18n.t('pickerPage.picker')
+            ],
             data: [
                 [pickData.data1]
             ]
+        }
+    },
+    methods: {
+        showPicker (index) {
+            this.$refs[`picker${index}`].show()
         }
     },
     components: {
@@ -36,4 +47,14 @@ export default {
 </script>
 
 <style scoped lang='less'>
+.picker-view {
+    .select {
+        margin: 0 10px 40px 10px;
+        height: 40px;
+        line-height: 40px;
+        border: 1px solid #e5e5e5;
+        text-align: center;
+        background: #fff;
+    }
+}
 </style>
